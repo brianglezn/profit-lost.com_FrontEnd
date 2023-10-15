2b58bdb8ffbb181a26e1528114cd3de0e5baf230
brianglezn <119687340+brianglezn@users.noreply.github.com>
2023-10-15 20:26:10 +0200

    15/10/23 *2
    Retoques de CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import DashBoard from './pages/DashBoard';
-import Cookies from './pages/Cookies';
-import ForgotPassword from './pages/ForgotPassword';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import DashBoard from "./pages/DashBoard";
+import Cookies from "./pages/Cookies";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   const theme = createTheme({
     palette: {
       primary: {
-        main: '#fe6f14',
+        main: "#fe6f14",
       },
       secondary: {
-        main: '#c84f03',
+        main: "#c84f03",
       },
       background: {
-        default: '#f7f7f7',
+        default: "#f7f7f7",
+      },
+      text: {
+        primary: "#383d46",
       },
     },
     typography: {
-      fontFamily: 'Rubik, sans-serif',
+      fontFamily: "Rubik, sans-serif",
     },
   });
 
@@ -32,12 +35,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/register' element={<Register />}></Route>
-          <Route path='/forgot-password' element={<ForgotPassword />}></Route>
-          <Route path='/app' element={<DashBoard />}></Route>
-          <Route path='/cookies' element={<Cookies />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/dashboard" element={<DashBoard />}></Route>
+          <Route path="/cookies" element={<Cookies />}></Route>
         </Routes>
       </ThemeProvider>
     </>
