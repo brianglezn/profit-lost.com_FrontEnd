04ed01380c21c8023b0a65f8bff2995dd7bfa3c0
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-04-04 17:50:31 +0200

    04/04/24
    AddMovementOK

--- src/components/dashboard/movements/FormMovementsAdd.tsx
+++ src/components/dashboard/movements/FormMovementsAdd.tsx
@@ -53,12 +53,11 @@ function FormMovementsAdd({ onMovementAdded, onClose }: FormMovementsAddProps) {
 
         const token = localStorage.getItem('token');
         if (!token) {
-            console.error('No authentication token found. Please log in.');
             toast.current?.show({
                 severity: 'error',
-                summary: 'Error',
+                summary: 'Authentication Error',
                 detail: 'No authentication token found. Please log in.',
-                life: 3000
+                life: 3000,
             });
             return;
         }
@@ -68,10 +67,19 @@ function FormMovementsAdd({ onMovementAdded, onClose }: FormMovementsAddProps) {
                 severity: 'warn',
                 summary: 'Validation Error',
                 detail: 'Amount must be a positive or negative number with up to two decimals.',
-                life: 3000
+                life: 3000,
             });
             return;
         }
+
+        const formattedDate = date ? `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}` : null;
+
+        const movementData = {
+            date: formattedDate,
+            description: description.trim() === '' ? '---' : description,
+            amount: parseFloat(amount.replace(',', '.')),
+            category: category ? category._id : null,
+        };
 
         try {
             const response = await fetch('https://profit-lost-backend.onrender.com/movements/add', {
@@ -80,12 +88,7 @@ function FormMovementsAdd({ onMovementAdded, onClose }: FormMovementsAddProps) {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}`,
                 },
-                body: JSON.stringify({
-                    date,
-                    description: description.trim() === '' ? '---' : description,
-                    amount: Number(amount),
-                    category: category?._id,
-                }),
+                body: JSON.stringify(movementData),
             });
 
             if (!response.ok) {
@@ -97,18 +100,20 @@ function FormMovementsAdd({ onMovementAdded, onClose }: FormMovementsAddProps) {
                 severity: 'success',
                 summary: 'Success',
                 detail: 'Movement added successfully',
-                life: 3000
+                life: 3000,
             });
+
             onMovementAdded();
             onClose();
         } catch (error) {
-            console.error('Error adding new movement:', error);
-            const message = error instanceof Error ? error.message : 'An error occurred';
+            const errorMessage = error instanceof Error ? error.message : 'An error occurred';
+
+            console.error('Error adding new movement:', errorMessage);
             toast.current?.show({
                 severity: 'error',
                 summary: 'Error',
-                detail: message,
-                life: 3000
+                detail: errorMessage,
+                life: 3000,
             });
         }
     };
@@ -117,7 +122,7 @@ function FormMovementsAdd({ onMovementAdded, onClose }: FormMovementsAddProps) {
         <>
             <Toast ref={toast} position="bottom-right" />
             <form onSubmit={handleSubmit} className="formMovements">
-                <Calendar value={date} className="form-calendar" onChange={(e) => setDate(e.value ? new Date(e.value) : null)} placeholder="Date" showTime required />
+                <Calendar value={date} dateFormat="dd/mm/yy" className="form-calendar" onChange={(e) => setDate(e.value ? new Date(e.value) : null)} placeholder="Date" showTime required />
                 <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" />
                 <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Amount" step="0.01" required />
                 <Dropdown value={category} options={categories} onChange={(e) => setCategory(e.value)} optionLabel="name" placeholder="Select a category" className="formMovements-category" required />
