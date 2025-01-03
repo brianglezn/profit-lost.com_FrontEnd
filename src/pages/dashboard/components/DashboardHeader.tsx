c501723fef11e54438a70193baa9b19243f1c297
Brian González Novoa <119687340+brianglezn@users.noreply.github.com>
2025-01-03 11:11:12 +0100

    Eliminamos svg.reload y hora del header

--- src/pages/dashboard/components/DashboardHeader.tsx
+++ src/pages/dashboard/components/DashboardHeader.tsx
@@ -1,11 +1,5 @@
 import React, { useEffect, useCallback } from 'react';
 import { Avatar } from 'primereact/avatar';
-import toast from 'react-hot-toast';
-import { useTranslation } from 'react-i18next';
-
-import { useUser } from '../../../context/useUser';
-import { getCurrentDate } from '../../../helpers/functions';
-import RefreshIcon from '../../../components/icons/RefreshIcon';
 
 import './DashboardHeader.scss';
 
@@ -20,14 +14,6 @@ const DashboardHeader: React.FC<DashboardHeaderProps> = ({
     userImage,
     userName,
 }) => {
-    const { t } = useTranslation();
-    const { user } = useUser();
-
-    const currentDate = getCurrentDate(
-        user?.language || 'en',
-        user?.dateFormat || 'DD/MM/YYYY',
-        user?.timeFormat || '24h'
-    );
 
     const handleScroll = useCallback(() => {
         const headerContainer = document.querySelector('.dashboard__header-container');
@@ -41,26 +27,9 @@ const DashboardHeader: React.FC<DashboardHeaderProps> = ({
         return () => window.removeEventListener('scroll', handleScroll);
     }, [handleScroll]);
 
-    const wakeUpBackend = async () => {
-        toast.promise(
-            fetch('https://sound-harlene-brian-novoa-be9c1292.koyeb.app/ping'),
-            {
-                loading: t('dashboard.dashboard.messages.loading'),
-                success: t('dashboard.dashboard.messages.reconnect_success'),
-                error: t('dashboard.dashboard.messages.reconnect_error'),
-            },
-            {
-                success: { duration: 2000 },
-                error: { duration: 2000 }
-            }
-        );
-    };
-
     return (
         <div className='dashboard__header'>
             <header className='dashboard__header-container'>
-                <RefreshIcon onClick={wakeUpBackend} />
-                <p className='dashboard__header-date'>{currentDate}</p>
                 <Avatar
                     onClick={onAvatarClick}
                     image={userImage ?? ''}
